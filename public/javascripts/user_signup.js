fbe9624939dac51349e9c2dfac9be2a0792811dd
Mark Wilson <mark@wilsonsdev.com>
2010-06-02 12:09:47 -0700

    cb work

--- public/javascripts/user_signup.js
+++ public/javascripts/user_signup.js
@@ -22,14 +22,49 @@ $(document).ready(function() {
 	 resetForm: false
  }
 );
+
+	function SeatChange() {
+			$('div#fed_seats').hide();
+
+			if (this.value == 'FederalCampaign') { 
+				$('div#fed_seats').css('display','inline'); 
+			}			
+		}
+
+	function SeatPopulate() {
+		$('div#seattype').hide();
+$('#user_organization_attributes_political_campaigns_attributes_0_seat_type').children().remove();
+
+		var fedOptions = { 
+"" : "Please select",		
+"U.S. Senate" : "U.S. Senate",
+"U.S. Congress" : "U.S. Congress" }
+
+		var stOptions = {
+			"" : "Please select",
+			"State Senate" : "State Senate",
+			"State House" : "State House"
+		}
+		var askSeat = false;
+
+		if (this.value == "FederalCampaign") {
+	$('#user_organization_attributes_political_campaigns_attributes_0_seat_type').addOption(fedOptions, false);
+		askSeat = true;
+	} else {
+		if (this.value == "StateCampaign") {
+$('#user_organization_attributes_political_campaigns_attributes_0_seat_type').addOption(stOptions, false);
+		askSeat = true;
+		}
+	}
+	
+	if (askSeat) {
+		$('div#seattype').css('display','inline');
+	}
+	
+	}
+	$('#user_organization_attributes_political_campaigns_attributes_0_type').bind("change", SeatPopulate);
+
 });
 
-//	function SeatChangeReset() {
-//			$('div#fed_seats').hide();
-
-//			if (this.value == 'FederalCampaign') { 
-//				$('div#fed_seats').css('display','inline'); 
-//			}			
-//		}
 
 
