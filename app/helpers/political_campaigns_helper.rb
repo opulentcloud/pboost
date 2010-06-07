4d267929881bc441b3222e7285d75fd14d2307d7
Mark Wilson <mark@wilsonsdev.com>
2010-06-07 14:13:47 -0700

    fix campaign edit links

--- app/helpers/political_campaigns_helper.rb
+++ app/helpers/political_campaigns_helper.rb
@@ -1,2 +1,13 @@
 module PoliticalCampaignsHelper
+
+	def get_edit_political_campaign_path(political_campaign)
+		case political_campaign.type
+		when 'FederalCampaign' then edit_federal_campaign_url(political_campaign.id)
+		when 'StateCampaign' then edit_state_campaign_url(political_campaign.id)
+		when 'CountyCampaign' then edit_county_campaign_url(political_campaign.id)
+		when 'MunicipalCampaign' then edit_municipal_campaign_url(political_campaign.id)
+		end
+
+	end
+	
 end
