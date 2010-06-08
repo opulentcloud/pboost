3235742f2fd6dfdab378adbdf639d41fc5d028a7
Mark Wilson <mark@wilsonsdev.com>
2010-06-08 12:54:30 -0700

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :application, "pboost"
+set :repository, "git@github.com:dstinnie/pboost.git"
+set :user, "netadm"
+set :sudo_password, "sudo password: "
+
+# If you have previously been relying upon the code to start, stop 
+# and restart your mongrel application, or if you rely on the database
+# migration code, please uncomment the lines you require below
+
+# If you are deploying a rails app you probably need these:
+
+# load 'ext/rails-database-migrations.rb'
+# load 'ext/rails-shared-directories.rb'
+
+# There are also new utility libaries shipped with the core these 
+# include the following, please see individual files for more
+# documentation, or run `cap -vT` with the following lines commented
+# out to see what they make available.
+
+# load 'ext/spinner.rb'              # Designed for use with script/spin
+# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/var/www/#{application}"
+
+#this config is for a local deployment
+#set :copy_strategy, :export
+#set :copy_cache, true
+#set :copy_cache, "/tmp/caches/pboost"
+#set :copy_exclude, [".git/*"]
+#set :copy_compression, :gzip
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+set :scm_username, "markysharky70"
+set :deploy_via, :remote_cache
+set :branch, "master"
+# see a full list by running "gem contents capistrano | grep 'scm/'"
+
+#role :web, "your web-server here"
+server "190.124.41.8 ", :app, :web, :db, :primary => true
+
+namespace :config do
+	desc "Link in config files"
+	task :link do
+		run "rm -f #{current_path}/config/database.yml && ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{current_path}/config/database.yml"
+		run "rm -f #{current_path}/config/email.yml && ln -nfs #{deploy_to}/#{shared_dir}/config/email.yml #{current_path}/config/email.yml"
+	end
+end
+
+deploy.task :restart, :roles => :app do
+	desc "Restart application"
+	run "touch #{current_path}/tmp/restart.txt"
+end
+
+after :deploy, "config:link"
+
+
