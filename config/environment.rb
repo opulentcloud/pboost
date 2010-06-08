bf37513b07affc374ab60d0ce057a148bf761e38
Mark Wilson <mark@wilsonsdev.com>
2010-06-08 15:10:00 -0700

    added geokit

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,8 @@ Rails::Initializer.run do |config|
 	config.gem "declarative_authorization", :source => 'http://gemcutter.org'
 	config.gem 'fastercsv'
 	config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+	config.gem "geokit"
+	config.gem 'postgis_adapter'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
