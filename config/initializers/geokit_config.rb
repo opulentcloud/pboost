acb30e47663915ef6eafcee1990a4c36e05cb71c
Mark Wilson <mark@wilsonsdev.com>
2010-06-09 10:42:59 -0700

    add google api key for IP 190.124.41.8 access

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -26,8 +26,7 @@ if defined? Geokit
 	# This is your Google Maps geocoder key. 
 	# See http://www.google.com/apis/maps/signup.html
 	# and http://www.google.com/apis/maps/documentation/#Geocoding_Examples
-	Geokit::Geocoders::google = 'ABQIAAAAP8PF3LMYnz-mbG7IgoZF6hQZwBjgfsfRkToaSbr0WUSUomvCZBQTOEt4-CqxsFZLuQiP
-XR2GQqgDMQ'
+	Geokit::Geocoders::google = 'ABQIAAAAP8PF3LMYnz-mbG7IgoZF6hRjPRKgfWxFnfLwJe0dsY4OAO08PBSs-wGRAhh5UsqbqTLd757jjS-IPw'
     
 	# This is your username and password for geocoder.us.
 	# To use the free service, the value can be set to nil or false.  For 
