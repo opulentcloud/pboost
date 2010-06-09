275ef232b61271fee55e477b53d8081c3b1180eb
Mark Wilson <mark@wilsonsdev.com>
2010-06-09 09:11:41 -0700

    add uuidtools

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,8 @@ Rails::Initializer.run do |config|
 	config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 	config.gem "geokit"
 	config.gem 'postgis_adapter'
-
+	config.gem 'uuidtools'
+	
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
