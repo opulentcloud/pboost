8766c5c3ad0f8dea1d147ba6c473f5971990ea1f
Mark Wilson <mark@wilsonsdev.com>
2010-06-15 15:02:13 -0700

    fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,17 +58,17 @@ end
 namespace :delayed_job do
 	desc "Start delayed_job process"
 	task :start, :roles => :app do
-		run "#{current_path}/script/delayed_job start RAILS_ENV=production"
+		run "RAILS_ENV=production #{current_path}/script/delayed_job start"
 	end
 	
 	desc "Stop delayed_job process"
 	task :stop, :roles => :app do
-		run "#{current_path}/script/delayed_job stop RAILS_ENV=production"
+		run "RAILS_ENV=production #{current_path}/script/delayed_job stop"
 	end
 	
 	desc "Restart delayed_job process"
 	task :restart, :roles => :app do
-		run "#{current_path}/script/delayed_job restart RAILS_ENV=production"
+		run "RAILS_ENV=production #{current_path}/script/delayed_job restart"
 	end
 
 end
