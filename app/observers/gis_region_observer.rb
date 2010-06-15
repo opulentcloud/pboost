aca12155c88fb39ca29daa8534d22321ac89fd12
Mark Wilson <mark@wilsonsdev.com>
2010-06-15 15:13:04 -0700

    new dj syntax

--- app/observers/gis_region_observer.rb
+++ app/observers/gis_region_observer.rb
@@ -4,7 +4,7 @@ class GisRegionObserver < ActiveRecord::Observer
 	def after_create(gis_region)
 
 		if @@delayed_job then
-			GisRegion.send_later(:populate_all_addresses_within, gis_region)
+			GisRegion.delay.populate_all_addresses_within gis_region
 		else
   		gis_region.populate_all_addresses_within
 		end
