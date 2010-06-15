f2958d7276a0ae55b1e297967de15ab53a713296
Mark Wilson <mark@wilsonsdev.com>
2010-06-15 14:57:16 -0700

    make gis_regions self populate

---
+++ app/observers/gis_region_observer.rb
@@ -0,0 +1,14 @@
+class MemberObserver < ActiveRecord::Observer
+	@@delayed_job = true
+
+	def after_create(gis_region)
+
+		if @@delayed_job then
+			GisRegion.send_later(:populate_all_addresses_within, gis_region)
+		else
+  		gis_region.populate_all_addresses_within
+		end
+	end	
+
+end
+
