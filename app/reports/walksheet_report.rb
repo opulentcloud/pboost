37da17f0d2753a270762ea986c3dd5c7f5268839
Mark Wilson <mark@wilsonsdev.com>
2010-06-24 13:31:56 -0700

    mew

--- app/reports/walksheet_report.rb
+++ app/reports/walksheet_report.rb
@@ -1,9 +1,8 @@
 
 class WalksheetReport
 
-	@@Widths = [50, 90, 170, 90, 90, 50] #portait is 540 width
-	@@Headers = ["Date", "Patient Name", "Description", "Charges / Payments", 
-				         "Patient Portion Due", "Balance"]
+	@@Widths = [340,200,200] #portait is 540 width
+	@@Headers = ["Patient Name", "Patient Portion Due", "Balance"]
 
 	def self.build(walksheet)
  
@@ -40,33 +39,32 @@ class WalksheetReport
 
 			data = []
 
-			def row(date, pt, charges, portion_due, balance)
-				rows = charges.map { |c| ["", "", c[0], c[1], "", ""] }
+			def row(pt, charges, portion_due, balance)
+				rows = charges.map { |c| ["", c[0], c[1]] }
 
-				# Date and Patient Name go on the first line.
-				rows[0][0] = date
-				rows[0][1] = pt
+				# Patient Name go on the first line.
+				rows[0][0] = pt
 
 				# Due and Balance go on the last line.
-				rows[-1][4] = portion_due
-				rows[-1][5] = balance
+				rows[-1][1] = portion_due
+				rows[-1][2] = balance
 
 				# Return a Prawn::Table object to be used as a subtable.
 				make_table(rows) do |t|
 				  t.column_widths = @@Widths
 				  t.cells.style :borders => [:left, :right], :padding => 2
-				  t.columns(4..5).align = :right
+				  t.columns(1..2).align = :right
 				end
 
 			end
 
-			data << row("1/1/2010", "", [["Balance Forward", ""]], "0.00", "0.00")
+			#data << row("", [["Balance Forward", ""]], "0.00", "0.00")
 			50.times do
-				data << row("1/1/2010", "John", [["Foo", "Bar"], 
+				data << row("John", [["Foo", "Bar"], 
 				                                 ["Foo", "Bar"]], "5.00", "0.00")
 			end
 
-			bounding_box [30,cursor], :width => 600 do
+			bounding_box [30,cursor], :width => 800 do
 				# Wrap head and each data element in an Array -- the outer table has only one
 				# column.
 					table([[head], *(data.map{|d| [d]})], :header => true,
