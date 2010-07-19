c0414c4842b04a98aa8302951c83e30435cccaf2
Mark Wilson <mark@wilsonsdev.com>
2010-07-19 11:38:52 -0700

    fix precinct bug

--- app/controllers/gis_regions_controller.rb
+++ app/controllers/gis_regions_controller.rb
@@ -36,7 +36,7 @@ class GisRegionsController < ApplicationController
   end
   
   def plot_precinct_cluster
-	 	@precinct = current_political_campaign.precincts.find_by_code(params[:precinct_code])
+	 	@precinct = current_political_campaign.precincts.find(params[:precinct_id].to_i)
   end
   
   def count_in_poly
