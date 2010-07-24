7a91183ea7e75cd626143aff8ea84596b3469c07
Mark Wilson <mark@wilsonsdev.com>
2010-07-24 12:03:21 -0700

    reports controller update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
 	before_filter :require_user
-	before_filter :get_walksheet, :only => [:show, :printable_list, :csv_list, :send_pdf_file]
+	before_filter :get_contact_list, :only => [:show, :printable_list, :csv_list, :send_pdf_file]
 
 	layout nil
 
@@ -16,9 +16,9 @@ class ReportsController < ApplicationController
 
 	def show
 		if RAILS_ENV == 'production'
-		send_file "#{RAILS_ROOT}/docs/canvass_list_#{@walksheet.id}.pdf", :type => "application/pdf", :x_sendfile => true
+		send_file "#{RAILS_ROOT}/docs/canvass_list_#{@contact_list.id}.pdf", :type => "application/pdf", :x_sendfile => true
 		else
-			send_file "#{RAILS_ROOT}/docs/canvass_list_#{@walksheet.id}.pdf", :type => "application/pdf"
+			send_file "#{RAILS_ROOT}/docs/canvass_list_#{@contact_list.id}.pdf", :type => "application/pdf"
 		end
 		return
 		#@lines_per_page = 28
@@ -32,7 +32,7 @@ class ReportsController < ApplicationController
 	end
 
 	def send_pdf_file
-		send_file "#{RAILS_ROOT}/docs/canvass_list_#{@walksheet.id}.pdf", :type => "application/pdf", :x_sendfile => true
+		send_file "#{RAILS_ROOT}/docs/canvass_list_#{@contact_list.id}.pdf", :type => "application/pdf", :x_sendfile => true
 	end
 
 	def printable_list
@@ -52,11 +52,11 @@ protected
 		WalkSheetReport.render(format, :walksheet => @walksheet.id)
 	end
 
-	def get_walksheet
+	def get_contact_list
 		begin
-    @walksheet = current_political_campaign.walksheets.find(params[:id])
+    @contact_list = current_political_campaign.contact_lists.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-    	flash[:error] = "The requested Canvass List was not found."
+    	flash[:error] = "The requested List was not found."
     	redirect_back_or_default customer_control_panel_url
     end
 	end
