1ce73405431166adc0752f8642d6b0b59f3cc073
Mark Wilson <mark@wilsonsdev.com>
2010-08-22 11:39:37 -0700

    wip

--- vendor/plugins/eschaton/slices/google_maps/google_view_ext.rb
+++ vendor/plugins/eschaton/slices/google_maps/google_view_ext.rb
@@ -25,7 +25,7 @@ module GoogleViewExt
     options.assert_valid_keys :key, :include_jquery
 
     jquery_script = self.include_jquery_javascript if options[:include_jquery] == true
-    google_script = "<script src=\"https://maps.google.com/maps?file=api&amp;v=2&amp;key=#{options[:key]}\" type=\"text/javascript\"></script>"
+    google_script = "<script src=\"http://maps.google.com/maps?file=api&amp;v=2&amp;key=#{options[:key]}\" type=\"text/javascript\"></script>"
 
     collect google_script, jquery_script,
             javascript_include_tag('eschaton')
