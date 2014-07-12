2aad1b9ab68c1d8457527479944874c68523e157
Mark Wilson <mark@wilsonsdev.com>
2014-07-12 15:04:37 -0700

    attempting to fix problems with rbenv path

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :rails_env, "production"
 set :deploy_via, :copy
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.2p95'
+set :rbenv_ruby, '2.1.2'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
