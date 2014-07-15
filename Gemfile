29af785bd9ea05b2a7877279959600b8e9a5de5d
Mark Wilson <mark@wilsonsdev.com>
2014-07-15 14:19:15 -0700

    wip

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 # Use unicorn as the app server
 gem 'unicorn'
+gem 'unicorn-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/deploy.rb
+++ config/deploy.rb
@@ -67,7 +67,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke "unicorn: start"
+      #invoke "unicorn: start"
+      run "/etc/init.d/unicorn restart"
     end
   end
 
