5c88dd9d224afcc15703fb044928f2f58a95bf7d
Mark Wilson <mark@wilsonsdev.com>
2014-07-16 10:14:52 -0700

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     root :to => "users/dashboard#show", :as => "authenticated_user_root"
   end
 
-  #root 'dashboard#show'
+  root 'dashboard#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
