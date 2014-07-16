48aaa4ea6523f605369730a5431aa89d94326a70
Mark Wilson <mark@wilsonsdev.com>
2014-07-15 18:12:56 -0700

    attempting to fix geom point on addresses table

--- db/migrate/20140716010154_update_geom_column_on_addresses.rb
+++ db/migrate/20140716010154_update_geom_column_on_addresses.rb
@@ -1,5 +1,10 @@
 class UpdateGeomColumnOnAddresses < ActiveRecord::Migration
-  def change
-    change_column :addresses, :geom, :point, geographic: true
+  def self.up
+    remove_column :addresses, :geom
+    add_column :addresses, :geom, :point, geographic: true
+  end
+  
+  def self.down
+    add_column :addresses, :geom, :point, srid: 4326
   end
 end
