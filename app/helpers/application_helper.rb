60e48e651349f5cdd143faac4983b0adbdb39df5
Mark Wilson <mark@wilsonsdev.com>
2014-07-20 09:46:00 -0700

    minor bug fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     fields = f.send("#{type}_fields", new_object, child_index: id) do |builder|
       render('voters/' + type.to_s + '_fields', f: builder)
     end
-    link_to('#', class: 'add_fields btn btn-default', data: {id: id, fields: fields.gsub('\n', '')}) do
+    link_to('#', style: "display: inline-block;", class: 'add_fields btn btn-default', data: {id: id, fields: fields.gsub('\n', '')}) do
       "#{name} #{content_tag(:span, "", class: "glyphicon glyphicon-plus-sign", title: "Add Search Condition")}".html_safe
     end
   end
