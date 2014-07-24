c0c42d21a629b8d37b56023cb4ee5dac39d47650
Mark Wilson <mark@wilsonsdev.com>
2014-07-23 23:55:01 -0700

    switch voter verify to dropdowns for mont/day of birth

--- app/controllers/customers/voter_verify_controller.rb
+++ app/controllers/customers/voter_verify_controller.rb
@@ -9,7 +9,8 @@ class Customers::VoterVerifyController < AuthenticatedUsersController
   
   def search
     @pg = params[:page] ||= 1
-    voter_dob = Date.parse("#{Date.today.year}-#{voter_params[:dob][0,2]}-#{voter_params[:dob][2,2]}") rescue ""
+    #voter_dob = Date.parse("#{Date.today.year}-#{voter_params[:dob][0,2]}-#{voter_params[:dob][2,2]}") rescue ""
+    voter_dob = Date.parse("#{Date.today.year}-#{voter_params[:birth_month]}-#{voter_params[:birth_day]}") rescue ""
     @search_index2 ||= Voter.build_search_index2(voter_params[:first_name], voter_params[:last_name],voter_dob)
     @voters = Voter.unscoped.where(search_index2: @search_index2).paginate(page: @pg, per_page: 1)
     flash.now[:notification] = "No records found for #{@search_index2}." unless @voters.total_entries > 0
@@ -20,6 +21,6 @@ class Customers::VoterVerifyController < AuthenticatedUsersController
   
 private
   def voter_params
-    @voter_params ||= params.require(:voter).permit(:first_name, :last_name, :street_no, :dob, :search_index, :search_index2)
+    @voter_params ||= params.require(:voter).permit(:first_name, :last_name, :street_no, :dob, :search_index, :search_index2, :birth_month, :birth_day)
   end
 end
