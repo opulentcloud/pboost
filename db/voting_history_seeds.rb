08c1f17c63d20874f24849045210d73621f5d77e
Mark Wilson <mark@wilsonsdev.com>
2016-01-31 14:53:56 -0800

    add voting_histories_populate! to VanData model

---
+++ db/voting_history_seeds.rb
@@ -0,0 +1,6 @@
+#=========== begin populate voter_histories table from van_data =========================
+
+  VanData.voting_histories_populate!
+
+  puts 'Finished populating voting_histories from van_data.'
+#=========== end populate voting_histories from van_data =========================
