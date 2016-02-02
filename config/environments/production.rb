1f525c5a4d63e8d3f6539ea34e6a377591c52b19
Mark Wilson <mark@wilsonsdev.com>
2016-02-01 17:40:33 -0800

    config action mailer to ignore ssl cert check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,8 @@ Rails.application.configure do
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'politicalboost.org',
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
   }
   config.action_mailer.default_url_options = { host: ENV['HOST_DOMAIN'] }
 
