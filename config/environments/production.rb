ce2612541afb1de8b9496cb22108254030304ec1
Mark Wilson <mark@wilsonsdev.com>
2016-02-01 16:50:01 -0800

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'mail.politicalboost.org',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'politicalboost.org',
     :enable_starttls_auto => true
   }
   config.action_mailer.default_url_options = { host: ENV['HOST_DOMAIN'] }
